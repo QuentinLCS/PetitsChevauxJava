82769dd68b93efade5d37fcc236c110d971ad317
Quentin <21802953@etu.unicaen.fr>
2019-05-14 22:40:56 +0200

    Création de la classe Pion.

--- src/domaine/Pion.java
+++ src/domaine/Pion.java
@@ -1,4 +1,15 @@
 package domaine;
 
 public class Pion {
+    private String id;
+    private Couleur couleur;
+
+    public Pion(String id, Couleur couleur) {
+        this.id = id;
+        this.couleur = couleur;
+    }
+
+    public Couleur getCouleur() {
+        return couleur;
+    }
 }
