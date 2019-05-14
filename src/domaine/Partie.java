6b943f55ad17129fc9aabf5b120661082a9af5cd
Nathan <21716210@etu.unicaen.fr>
2019-05-15 00:32:48 +0200

    Ajout des méthodes non développées de Plateau.java et Partie.java

--- src/domaine/Partie.java
+++ src/domaine/Partie.java
@@ -1,4 +1,67 @@
 package domaine;
 
+import exceptions.*;
+import java.util.ArrayList;
+import java.util.Random;
+
 public class Partie {
+
+    private Random de = new Random() ;
+    private Joueur joueurCourant;
+    private ArrayList<Joueur> joueurs;
+    private Plateau plateau;
+
+    public Partie() {
+    }
+
+    public void initialiserJoueurs(int nbJoueur){
+        if (!(nbJoueur<0 || nbJoueur>4)) {
+            joueurs = new ArrayList<>(nbJoueur);
+        }
+    }
+
+    public void initialiserPlateau(){
+        plateau = new Plateau();
+    }
+
+    private int lancerDe(){
+        return de.nextInt(6)+1;
+    }
+
+    public void jouerUnTour(){
+        //methode à completer
+    }
+
+    public boolean estPartieTermine(){
+        //methode à completer ----> vérifier les règles
+        return true;
+    }
+
+    public Joueur getJoueurCourant() {
+        return joueurCourant;
+    }
+
+    public void setJoueurCourant(Joueur joueurCourant) {
+        this.joueurCourant = joueurCourant;
+    }
+
+    public ArrayList<Joueur> getJoueurs() {
+        return joueurs;
+    }
+
+    public Plateau getPlateau() {
+        return plateau;
+    }
+
+    private void mangerLesPions(Case cs){
+        ArrayList<Pion> chevaux = cs.getChevaux();
+        ArrayList<CaseEcurie> ecuries = getPlateau().getEcuries();
+        for (Pion pion:chevaux) {
+            for (CaseEcurie ecu:ecuries){
+                if (ecu.getCouleur()==pion.getCouleur()) {
+                    getPlateau().deplacerPionA(pion, ecu);
+                }
+            }
+        }
+    }
 }
--- src/domaine/Plateau.java
+++ src/domaine/Plateau.java
@@ -1,4 +1,35 @@
 package domaine;
 
+import java.util.ArrayList;
+
 public class Plateau {
+
+    private ArrayList<ArrayList<CaseDEchelle>> echelles;
+    private ArrayList<CaseDeChemin> chemin;
+    private ArrayList<CaseEcurie> ecuries;
+
+    public Plateau() {
+    }
+
+    public ArrayList<ArrayList<CaseDEchelle>> getEchelles() {
+        return echelles;
+    }
+
+    public ArrayList<CaseDeChemin> getChemin() {
+        return chemin;
+    }
+
+    public ArrayList<CaseEcurie> getEcuries() {
+        return ecuries;
+    }
+
+    public void afficher(){
+        //A faire ou non selon si on JavaFX or not lol
+    }
+
+    public void deplacerPionA(Pion p, Case c){
+        int pos=chemin.indexOf(p);
+        chemin.get(pos).getChevaux().remove(p);
+        c.ajouteCheval(p);
+    }
 }
