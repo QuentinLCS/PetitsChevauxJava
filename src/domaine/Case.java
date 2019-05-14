fe2387b187474cfae8b2654ad3e0264f3777b4cd
Quentin <21802953@etu.unicaen.fr>
2019-05-14 21:59:21 +0200

    Création de la classe Case.

--- src/domaine/Case.java
+++ src/domaine/Case.java
@@ -1,4 +1,23 @@
 package domaine;
 
-public class Case {
+import java.util.ArrayList;
+
+public abstract class Case {
+    private ArrayList<Pion> chevaux;
+
+    public Case(ArrayList<Pion> chevaux) {
+        this.chevaux = chevaux;
+    }
+
+    public ArrayList<Pion> getChevaux() {
+        return chevaux;
+    }
+
+    public void ajouteCheval(Pion cheval) {
+        chevaux.add(cheval);
+    }
+
+    public abstract boolean peutPasser(Pion cheval);
+
+    public abstract boolean peutSArreter(Pion cheval);
 }
