72b735e8eeb01848f4c20e398fdde7c014ab4cc8
Quentin <21802953@etu.unicaen.fr>
2019-05-14 21:44:02 +0200

    Création des exceptions

---
+++ src/exceptions/ConflitDeCouleurException.java
@@ -0,0 +1,7 @@
+package exceptions;
+
+public class ConflitDeCouleurException extends Exception {
+    public ConflitDeCouleurException() {
+        super();
+    }
+}
---
+++ src/exceptions/PasDeJoueurException.java
@@ -0,0 +1,8 @@
+package exceptions;
+
+public class PasDeJoueurException extends Exception {
+    public PasDeJoueurException() {
+        super();
+
+    }
+}
