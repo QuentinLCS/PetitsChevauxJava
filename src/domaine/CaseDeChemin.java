b4da62a0cdf5ccef51e2d1e7ee1b37475d4a5aa2
Nathan <21716210@etu.unicaen.fr>
2019-05-17 00:33:57 +0200

    ajout exception caseChemin et modif de mangerLesPions dans Partie.java

--- src/domaine/CaseDeChemin.java
+++ src/domaine/CaseDeChemin.java
@@ -1,11 +1,14 @@
 package domaine;
 
+import exceptions.ConflitDeCouleurException;
+
 public class CaseDeChemin extends Case {
     public CaseDeChemin() {
         super();
     }
 
-    public boolean peutPasser(Pion cheval) {
+    public boolean peutPasser(Pion cheval) throws ConflitDeCouleurException {
+        if (!(cheval.getCouleur().equals(this.getChevaux().get(0).getCouleur()))) throw new ConflitDeCouleurException();
         return this.getChevaux().isEmpty() || cheval.getCouleur().equals(this.getChevaux().get(0).getCouleur());
     }
 
