cc8ca6d6090da50a0bba1783e414cbb5b491c2bd
Quentin <21802953@etu.unicaen.fr>
2019-05-17 23:50:03 +0200

    Add JavaDoc.

--- src/domaine/Couleur.java
+++ src/domaine/Couleur.java
@@ -17,6 +17,7 @@ public enum Couleur {
      * Le constructeur d'un objet Couleur. Les codes couleurs sont à disposer avant le message devant recevoir la coloration. Fermer la coloration avec "\033[0m".
      * @param codeCouleur Code couleur sur fond blanc de la couleur.
      * @param codeCouleurFond Code couleur blanc sur fond de la couleur.
+     * @param indexCaseDepart Valeur de l'index dans la liste chemin de la case de départ pour une couleur donnée.
      */
     Couleur(String codeCouleur, String codeCouleurFond, int indexCaseDepart) {
         this.codeCouleur = codeCouleur;
